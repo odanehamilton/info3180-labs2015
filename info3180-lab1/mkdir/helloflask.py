5b2fe9d223e5b9f6c2f5306fae467515f90c2e00
Odane P. Hamilton <odane.hamilton@gmail.com>
2016-04-02 02:44:44 +0000

    Initial commit

---
+++ info3180-lab1/mkdir/helloflask.py
@@ -0,0 +1,13 @@
+import os
+HOST=os.environ['IP']
+PORT=os.environ['PORT']
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return """<html><head></head><body>Hello World!"</body></html>"""
+
+if __name__ == "__main__":
+    app.run(host=HOST,port=PORT)
